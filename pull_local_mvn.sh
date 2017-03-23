d3f19a1850302c250c0a344705ca6356ec46a141
Damien Zufferey <zufferey@mpi-sws.org>
2017-03-23 18:00:13 +0100

    small update

--- pull_local_mvn.sh
+++ pull_local_mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x #echo on
-rsync -arvP -i --delete $HOME/.m2/repository/ repository/
+rsync -arvP -i --delete $HOME/.m2/repository/io/github/dzufferey/ repository/io/github/dzufferey/
